fac7b40a43fd3ab786482884930db20c3b3e8abc
Tiffany <marintiffany3@gmail.com>
2025-07-30 10:11:29 +0200

    page error

--- my-react-app/src/pages/Error.jsx
+++ my-react-app/src/pages/Error.jsx
@@ -1,3 +1,21 @@
+// export default function Error() {
+//   return <h1>404 - Oups! La page que vous demandez n'existe pas.</h1>;
+// }
+
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/_error.scss";
+
 export default function Error() {
-  return <h1>404 - Oups! La page que vous demandez n'existe pas.</h1>;
+  return (
+    <>
+      <Header />
+      <main className="error">
+        <h1 className="error__code">404</h1>
+        <p className="error__message">Oups! La page que vous demandez n'existe pas.</p>
+        <a href="/" className="error__link">Retourner sur la page d’accueil</a>
+      </main>
+      <Footer />
+    </>
+  );
 }
