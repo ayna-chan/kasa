c706f71f4eff51c7f9ec1e9603f3e3ee3226cd46
Tiffany <marintiffany3@gmail.com>
2025-08-16 10:10:43 +0200

    détails logement

--- my-react-app/src/pages/Logement.jsx
+++ my-react-app/src/pages/Logement.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import data from "../data/data.json";
 import Slideshow from "../components/Slideshow";
+import Collapse from "../components/Collapse";
+import "../styles/_logement.scss";
 
 export default function Logement() {
   const { id } = useParams();
@@ -10,11 +12,65 @@ export default function Logement() {
     return <h2>Logement introuvable</h2>;
   }
 
+  // Séparation prénom / nom sinon impossible d'empiler
+  const [firstName, lastName] = logement.host.name.split(" ");
+
   return (
-    <div>
+    <div className="logement-container">
       <Slideshow pictures={logement.pictures} />
-      <h1>{logement.title}</h1>
-      <p>{logement.location}</p>
+
+      <div className="logement-header">
+        <div className="logement-info">
+          <h1>{logement.title}</h1>
+          <p className="location">{logement.location}</p>
+          <div className="tags">
+            {logement.tags.map((tag, index) => (
+              <span key={index} className="tag">{tag}</span>
+            ))}
+          </div>
+        </div>
+
+        <div className="host-rating">
+          <div className="host">
+            <div className="host-name">
+              <span>{firstName}</span>
+              <span>{lastName}</span>
+            </div>
+            <img
+              src={logement.host.picture}
+              alt={logement.host.name}
+              className="host-picture"
+            />
+          </div>
+          <div className="rating">
+            {Array.from({ length: 5 }, (_, i) => (
+              <span
+                key={i}
+                className={i < logement.rating ? "star filled" : "star"}
+              >
+                ★
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="collapse-section">
+        <div className="collapse-item">
+          <Collapse title="Description">
+            <p>{logement.description}</p>
+          </Collapse>
+        </div>
+        <div className="collapse-item">
+          <Collapse title="Équipements">
+            <ul>
+              {logement.equipments.map((eq, index) => (
+                <li key={index}>{eq}</li>
+              ))}
+            </ul>
+          </Collapse>
+        </div>
+      </div>
     </div>
   );
 }
